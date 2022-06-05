7c5b151085e8a045792439a8a783d90236c4970b
Amin Mahmudian <amin.mahmudian@gmail.com>
2022-06-05 12:52:56 +0430

    using the `calculate` lib in main package

--- bin/first_dart_project.dart
+++ bin/first_dart_project.dart
@@ -1,5 +1,9 @@
+// @dart=2.9
 import 'package:first_dart_project/first_dart_project.dart' as first_dart_project;
 
 void main(List<String> arguments) {
-  print('Hello world: ${first_dart_project.calculate()}!');
+  print('Add: ${first_dart_project.calculateAdd()}');
+  print('Dvide: ${first_dart_project.calculateDivide()}');
+  print('Multiply: ${first_dart_project.calculateMultiply()}');
+  print('Subtract: ${first_dart_project.calculateSubtract()}');
 }
--- lib/first_dart_project.dart
+++ lib/first_dart_project.dart
@@ -1,3 +1,14 @@
-int calculate() {
-  return 6 * 7;
+import 'package:calculator/calculator.dart';
+
+int calculateAdd() {
+  return add(6, 7);
+}
+double calculateDivide() {
+  return divide(6, 7);
+}
+int calculateMultiply() {
+  return multiply(6, 7);
+}
+int calculateSubtract() {
+  return subtract(6, 7);
 }
